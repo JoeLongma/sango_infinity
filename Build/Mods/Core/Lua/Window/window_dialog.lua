f53daa1fa0c19f0ea71f1de79d3b2e5c5f9fbf4b
tank <1052058@qq.com>
2025-10-09 18:15:02 +0800

    弹窗1秒自动关闭

--- Build/Mods/Core/Lua/Window/window_dialog.lua
+++ Build/Mods/Core/Lua/Window/window_dialog.lua
@@ -6,18 +6,18 @@ Window_Dialog = Class(Window)
 
 function Window_Dialog:Awake()
     self.ContentText = self:GetText("contentBg/content");
-    local co = nil;
-    co = coroutine.start(function()
-        coroutine.wait(3);
-        self.mEvent.IsDone = true;
-        self.mEvent = nil;
-        coroutine.stop(co);
-    end)
 end
 
 function Window_Dialog:InitEvent(event, content)
     self.mEvent = event;
     self.ContentText.text = content;
+    local co = nil;
+    co = coroutine.start(function()
+        coroutine.wait(1);
+        self.mEvent.IsDone = true;
+        self.mEvent = nil;
+        coroutine.stop(co);
+    end)
 end
 
 function Window_Dialog:OnButton_ok()
